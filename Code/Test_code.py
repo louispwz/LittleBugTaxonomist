da6036274a6940d1e0df617f6811c424ea94115a
Louis pwz <louis.pwz@gmail.com>
2025-12-02 14:17:13 +0100

    update code

--- Code/Test_code.py
+++ Code/Test_code.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 import open_clip
 import torch
-import torch.nn.functional as F
+import torch.nn.functional
 
 # verifie si GPU disponible (mais moi je n'ai pas)
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -28,12 +28,12 @@ def predict_image_zero_shot(image_path, candidate_labels): # en entrée une imag
         image_features = model.encode_image(image_input)
 
     # prompt pour chaque photo
-    texts = [f"a photo of a {label}" for label in candidate_labels]
-    text_tokens = tokenizer(texts).to(device)
+    textes = [f"a photo of a {label}" for label in candidate_labels]
+    texte_tokens = tokenizer(textes).to(device)
 
     # Embedding du texte
     with torch.no_grad():
-        text_features = model.encode_text(text_tokens)
+        text_features = model.encode_text(texte_tokens)
 
     # norm
     image_features = image_features / image_features.norm(dim=-1, keepdim=True)
@@ -45,8 +45,8 @@ def predict_image_zero_shot(image_path, candidate_labels): # en entrée une imag
 
     # Trie les labels par proba decroissante (meilleur en premier)
     probs = probs.cpu().numpy()
-    sorted_idx = probs.argsort()[::-1]
-    return [(candidate_labels[i], float(probs[i])) for i in sorted_idx]
+    tri_index = probs.argsort()[::-1]
+    return [(candidate_labels[i], float(probs[i])) for i in tri_index]
 
 
 
@@ -54,12 +54,12 @@ def predict_image_zero_shot(image_path, candidate_labels): # en entrée une imag
 
 if __name__ == "__main__":
     # image
-    # image_path = "Code/Test_images/images_raw/00014904.jpg"
-    image_path = "Code/Test_images/images_raw/00013600.jpg"
-    # image_path = "Code/Test_images/images_raw/00000053.jpg"
+    # image_path = "Code/Test_images/images_raw/00014500.jpg"
+    # image_path = "Code/Test_images/images_raw/00013600.jpg"
+    image_path = "Code/Test_images/images_raw/00000053.jpg"
 
     # Liste des labels candidats un peu mis au hasard mais on peut le changer si on sait ce qu'on fait
-    candidate_labels = ["grass-hopper", "mantis religiosa", "bee", "dog", "butterfly", "Danaus plexippus", "beetle"]
+    candidate_labels = ["grass-hopper", "mantis religiosa", "bee", "dog", "butterfly", "Danaus plexippus", "beetle", "Animalia Arthropoda Insecta Lepidoptera Nymphalidae Danaus Plexippus"]
 
     # Pred zeroshot
     result = predict_image_zero_shot(image_path, candidate_labels)
